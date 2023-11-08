03b21345e2ce1db6bd01a77924c01919ed6c6923
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2023-11-08 12:57:18 +0000

    nav bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+import classes from "./Header.module.css";
+
+function header() {
+  return (
+    <nav>
+      <div className={classes.teste}>
+        <div>
+          <h1 className={classes.title}>Bookie</h1>
+        </div>
+        <div className={classes.teste}>
+          <input
+            type="text"
+            placeholder="Search.."
+            className={classes.searchBar}
+          />
+          <button className={classes.searchBarButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className={classes.teste}>
+          <button className={classes.cart}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.6}
+              stroke="currentColor"
+              className="w-2.5 h-2.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+            <div>$35.19</div>
+          </button>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <ul className={`${classes.teste} ${classes.lista}`}>
+          <li>
+            <Link className={classes.lista} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={classes.lista} to="/new-meetup">
+              New releases
+            </Link>
+          </li>
+          <li>
+            <Link className={classes.lista} to="/favorites">
+              Recomendations
+            </Link>
+          </li>
+          <li>
+            <Link className={classes.lista} to="/favorites">
+              Recomendations
+            </Link>
+          </li>
+          <li>
+            <Link className={classes.lista} to="/favorites">
+              Recomendations
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default header;
